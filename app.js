5c97b9686fdcce934de5425e67ad6045ab8f459c
Terenz Jahred Dantes <terenzdantes717@gmail.com>
2025-11-22 18:19:58 +0800

    Made the logic work

---
+++ app.js
@@ -0,0 +1,112 @@
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+const addButton = document.querySelector('.add-button');
+
+addButton.addEventListener('click', () => {
+    addTask();
+})
+
+function displayTask() {
+    const list = document.querySelector('.task-list');
+
+    todoListHTML = '';
+
+    tasks.forEach((task) => {
+        
+        const taskHTML = `
+            <li class="task"">
+                    <input class="checkbox" type="checkbox" id="task">
+                    <label for="task">${task.taskName}</label>
+                    <button class="delete-task">Delete</button>
+            </li>
+        `
+    todoListHTML += taskHTML;
+
+    })
+
+    list.innerHTML = todoListHTML;
+
+    document.querySelectorAll('.delete-task').forEach((button, index) => {
+        button.addEventListener('click', () => {
+            tasks.splice(index, 1);
+            displayTask();
+        })
+    })
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function addTask() {
+    const taskInput = document.getElementById("input-box");
+
+    if (taskInput.value) {
+        const taskName = taskInput.value;
+        tasks.push({taskName})
+        console.log(tasks); 
+
+    } else {
+        throw new Error ('No input');
+    }
+    
+    displayTask();
+    taskInput.value = "";
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+
+
+displayTask();
+
+
+
+
+
+
+
+
+
+// function displayTask () {
+   
+
+// }
+
+
+
+
+// function addTask(task) {
+
+    
+//     taskHTML += `
+//                 <li class="task">
+//                     <input class="checkbox" type="checkbox" id="task">
+//                     <label for="task">${task}</label>
+//                     <button class="delete-task">Delete</button>
+//                 </li>
+//                 `;
+//     displayTask();
+
+
+//         // const list = document.querySelector('.task-list');
+//         // const li = document.createElement("li");
+//         // li.classList.add("task");
+
+//         // li.innerHTML = `
+//         //                 <input class="checkbox" type="checkbox" id="task">
+//         //                 <label for="task">${task}</label>
+//         //                 <button class="delete-task">Delete</button>
+//         //             `;
+//         // list.append(li);
+    
+//     // const deleteButton = li.querySelector('.delete-task');
+//     // deleteButton.addEventListener('click', deleteTask);
+// }
+    
+
+
+
+    
+
+// function deleteTask(index) { 
+//         console.log('Deleted.');
+//         tasks.splice(index,1);
+//         console.log(tasks);
+// }
